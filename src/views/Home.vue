9983bf6d75c15f7eb38d813de465bc30406d8208
Jean Loberg Hansen <jean@lait.dk>
2019-12-21 13:04:57 +0100

    Created threejsHover new

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <HelloWorld msg="ThreeJs hover"/>
+    <HelloWorld msg="ThreeJs hover" />
+
+    <div class="test-area">
+      <h2>Test area</h2>
+      <div class="test-dis"></div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import { gsap } from "gsap";
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue";
+import gsap from "gsap";
+import * as THREE from "three";
+// @ts-ignore
+import hoverEffect from "hover-effect";
+@Component({
+  components: { HelloWorld }
+})
+export default class Home extends Vue {
+  @Prop() private msg!: string;
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  mounted() {
+    const test = new hoverEffect({
+      parent: document.querySelector(".test-dis"),
+      intensity: 0.2,
+      image1: "https://picsum.photos/id/1052/3900/3120",
+      image2: "https://picsum.photos/id/1049/3900/3120",
+      displacementImage: "https://i.loli.net/2019/09/19/BgYpePLsGiCJ8AI.png"
+    });
+    return test;
   }
 }
 </script>
+<style scoped lang="scss">
+.test-dis {
+  height: 600px;
+  width: 600px;
+}
+img {
+  max-width: 80%;
+}
+</style>
