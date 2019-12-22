8c0472f7976ab985cb685b42d427918e4ceb5058
Jean Loberg Hansen <jean@lait.dk>
2019-12-22 22:43:30 +0100

    Smooth shit
    Smooth transitions

--- src/assets/scripts/custom/PageTransitionHandler.ts
+++ src/assets/scripts/custom/PageTransitionHandler.ts
@@ -2,21 +2,20 @@ export default class PageTransition {
     constructor() {
     }
     public beforeEnter = (el: HTMLElement | undefined) => { // before new page enter
-            el.style.transform = "translateY(100%)";
-            el.style.visibility = "hidden";
+        el.style.transform = "translateY(100%)";
+        el.style.zIndex = "1";
     }
     public enter = (el: HTMLElement | undefined) => { // new page enter
         setTimeout(() => {
             el.style.transitionDelay = ".1s";
             el.style.opacity = "1";
+            el.style.zIndex = "2";
             el.style.transform = "translateY(0)";
-            el.style.visibility = "visible";
         }, 10);
     }
     public leave = (el: HTMLElement | undefined) => { // current page leaves
-        el.style.bottom = "0";
-            el.style.opacity = "0.5";
-            el.style.transform = "translateY(-50%)";
-     
+        el.style.opacity = "0.5";
+        el.style.zIndex = "1";
+        el.style.transform = "translateY(-50%)";
     }
 }
